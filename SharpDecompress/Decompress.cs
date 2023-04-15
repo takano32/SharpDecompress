f5adbf53e76beba805d2e3e58643af9ef6427919
TAKANO Mitsuhiro <takano32@gmail.com>
2023-04-15 11:44:25 +0900

    Add Decompress class

---
+++ SharpDecompress/Decompress.cs
@@ -0,0 +1,31 @@
+﻿
+using SharpCompress.Common;
+using SharpCompress.Archives;
+
+public class Decompress
+{
+	public Decompress()
+	{
+	}
+
+	public void Process()
+	{
+		Console.WriteLine("Decompress::Process");
+        using (Stream stream = File.OpenRead("/Users/takano32/Downloads/Archive.7z"))
+        using (var archive = ArchiveFactory.Open(stream))
+        {
+            foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+            {
+                entry.WriteToDirectory("/Users/takano32/tmp", new ExtractionOptions()
+                {
+                    ExtractFullPath = true,
+                    Overwrite = true
+                });
+            }
+        }
+
+    }
+
+}
+
+
--- SharpDecompress/Program.cs
+++ SharpDecompress/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
+
+
+Decompress dec = new Decompress();
+dec.Process();
 
